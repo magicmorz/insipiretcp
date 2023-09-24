735846346610ee8ab0896ab9463c7efb0bad4f71
Michael Morozov <morozov101@gmail.com>
2023-09-24 16:22:17 +0300

    created GetInterfaceIndex function

--- insipiretcp/socket_utils.c
+++ insipiretcp/socket_utils.c
@@ -19,27 +19,30 @@ int CreateRawSocket(int protocol_to_sniff) {
     return sock_desc;
 }
 
-void BindRawSocketToInterface(int sock_desc, char *interface_name) {
-    struct sockaddr_ll packet_info;
-    struct ifreq ifr;
-
-    memset(&packet_info, 0, sizeof(struct sockaddr_ll));
-    memset(&ifr, 0, sizeof(struct ifreq));
-
-    // Get the index of the interface
-    strncpy(ifr.ifr_name, interface_name, IFNAMSIZ - 1);
-    if (ioctl(sock_desc, SIOCGIFINDEX, &ifr) == -1) {
-        perror("SIOCGIFINDEX");
+int GetInterfaceIndex(int sock_desc, char *interface_name)
+{
+    struct ifreq interface_request;
+    memset(&interface_request, 0, sizeof(struct ifreq));
+    strncpy(interface_request.ifr_name, interface_name, IFNAMSIZ - 1);
+    // ioctl will make interface_request.ifr_ifindex filled by the kernel 
+    if (ioctl(sock_desc, SIOCGIFINDEX, &interface_request) == -1) {   
+        perror("Error getting interface index");
         close(sock_desc);
         exit(EXIT_FAILURE);
     }
+    return interface_request.ifr_ifindex;
+}   
 
-    packet_info.sll_family = AF_PACKET;
-    packet_info.sll_protocol = htons(ETH_P_ALL); // Capture all Ethernet protocols, endieness (host to network byte order, short)
-    packet_info.sll_ifindex = ifr.ifr_ifindex;
+void BindRawSocketToInterface(int sock_desc, char *interface_name) {
+    struct sockaddr_ll socket_info;
+    memset(&socket_info, 0, sizeof(struct sockaddr_ll));
+   
+    socket_info.sll_family = AF_PACKET;
+    socket_info.sll_protocol = htons(ETH_P_ALL); // Capture all Ethernet protocols, endieness (host to network byte order, short)
+    socket_info.sll_ifindex = GetInterfaceIndex(sock_desc, interface_name);
 
     // Bind the socket to the specified network interface
-    if (bind(sock_desc, (struct sockaddr *)&packet_info, sizeof(struct sockaddr_ll)) == ERROR) {
+    if (bind(sock_desc, (struct sockaddr *)&socket_info, sizeof(struct sockaddr_ll)) == ERROR) {
         perror("Bind error");
         close(sock_desc);
         exit(EXIT_FAILURE);
